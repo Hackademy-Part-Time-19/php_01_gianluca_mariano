47bad80ed0a1d5767fd3aa6bbf2a23752caa4f92
Gianluca Mariano <giangimar04@gmail.com>
2024-01-09 18:51:57 +0100

    Ver 1.0

---
+++ es-1.php
@@ -0,0 +1,20 @@
+
+
+
+<?php 
+
+
+// Esercizio 1 
+
+$var_int = 32;
+$var_float = 20.23;
+$var_stringa = "Giovanni ha 39 anni.";
+$var_bool = true;
+
+var_dump($var_int, $var_float, $var_stringa, $var_bool);
+
+
+// Esercizio 2
+
+
+
